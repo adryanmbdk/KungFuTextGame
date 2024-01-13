1198a001a6bbcc02065248ebcb1bf796db0a3b21
adryanmbdk <adryanmbs@gmail.com>
2024-01-13 20:58:28 -0300

    dojo criado, falta finalizar

--- Player.py
+++ Player.py
@@ -1,7 +1,9 @@
 class Player:
-    def __init__(self, grana, energiaMax, poder, forca, agilidade):
+    def __init__(self, grana, energiaMax, recuperacao, poder, forca, agilidade, nivel):
         self.grana = grana    
         self.energiaMax = energiaMax 
+        self.recuperacao = recuperacao
         self.poder = poder
         self.forca = forca
         self.agilidade = agilidade
+        self.nivel = nivel
--- main.py
+++ main.py
@@ -6,52 +6,56 @@ import math, random, time
 from Player import Player
 
 ## variaveis
-player = Player(grana=0, energiaMax=20, poder=2, forca=1, agilidade=1)
+player = Player(grana=0, energiaMax=20, recuperacao=1,poder=2, forca=1, agilidade=1, nivel="Encrenqueiro")
 energia = player.energiaMax
 recuperacao_em_andamento = False
-recuperacao = 1
-recuperacaoAux = recuperacao
+recuperacaoAux = player.recuperacao
+corNivel="black"
+corNivelBg="white"
+
+tempoPastel = 0
+tempoBaterCarteira = 0
 
 ## funções
-def atualizar(energia, energiaMax, forca, agilidade, grana):
+def atualizar(energia):
     player.poder = player.forca + player.agilidade
     poder = round((player.forca + player.agilidade),2)
-    label_energia.config(text="Energia: " + str(energia) + "/" + str(energiaMax))
-    label_grana.config(text="Grana: " + str(grana))
+    label_energia.config(text="Energia: " + str(energia) + "/" + str(player.energiaMax))
+    label_grana.config(text="Grana: " + str(player.grana))
     label_poder.config(text="Poder: " + str(poder))
-    label_forca.config(text="Força: " + str(forca))
-    label_agilidade.config(text="Agilidade: " + str(agilidade))
+    label_forca.config(text="Força: " + str(player.forca))
+    label_agilidade.config(text="Agilidade: " + str(player.agilidade))
 
 def flexao():
-    global energia, recuperacao_em_andamento, recuperacao
+    global energia, recuperacao_em_andamento
     condicao = energia - 10
     if condicao >= 0:
         energia -= 10
         ## player.forca = round((player.forca + player.forca * 0.0325),2)  ## calculo de quanto de força que ganha a cada flexao
         aumentarForca(1)
-        atualizar(energia, player.energiaMax, player.forca, player.agilidade, player.grana)
+        atualizar(energia)
         if not recuperacao_em_andamento:
             recuperarEnergia()
 
 def agachamento():
-    global energia, recuperacao_em_andamento, recuperacao
+    global energia, recuperacao_em_andamento
     condicao = energia - 10
     if condicao >= 0:
         energia -= 10
         aumentarAgilidade(1)
-        atualizar(energia, player.energiaMax, player.forca, player.agilidade, player.grana)
+        atualizar(energia)
         if not recuperacao_em_andamento:
             recuperarEnergia()
 
 def recuperarEnergia():
-    global energia, recuperacao_em_andamento, recuperacao
+    global energia, recuperacao_em_andamento
     if energia < player.energiaMax:
         recuperacao_em_andamento = True
-        if energia + recuperacao > player.energiaMax:
+        if energia + player.recuperacao > player.energiaMax:
             energia = player.energiaMax
         else:
-            energia += recuperacao
-        atualizar(energia, player.energiaMax, player.forca, player.agilidade, player.grana)
+            energia += player.recuperacao
+        atualizar(energia)
         janela.after(1000, recuperarEnergia)    
     else:
         recuperacao_em_andamento = False
@@ -73,29 +77,70 @@ def aumentarForca(num):
         player.forca += num
     
 def aumentarAgilidade(num):
-    global recuperacao
     if (player.agilidade + 1) % 25 == 0 and player.agilidade != 1:
         player.agilidade += num
-        recuperacao += 1
+        player.recuperacao += 1
     else:
         player.agilidade += num
 
 def baterCarteira():
+    global tempoBaterCarteira
+    tempoAtual = time.time()
+    resta = tempoAtual - tempoBaterCarteira
     chance = random.randint(1, 100)
     condicao = energia - 10
-    if chance > 80 and condicao >= 0:
-        messagebox.showinfo("Alguém te viu!", "Você foi quase foi pego batendo carteira!\n \t-10 de ENERGIA")
-        perderEnergia(10)
-    elif chance <= 80 and condicao >= 0:
-        chance2 = random.randint(1, 100)
-        if chance2 < 80:
-            grana = random.randint(10,20)
-        else:
-            grana = random.randint(35,50)
-        player.grana += grana
-        messagebox.showinfo("Ladrãozinho!", "Você conseguiu roubar uma carteira e ganhou "+ str(grana)+ " reais!\n \t-10 de ENERGIA")
-        perderEnergia(10)
-    atualizar(energia, player.energiaMax, player.forca, player.agilidade, player.grana)
+    if resta >= 20:
+        tempoBaterCarteira = 0
+        if chance > 80 and condicao >= 0:
+            messagebox.showinfo("Alguém te viu!", "Você foi quase foi pego batendo carteira!\n \t-10 de ENERGIA")
+            tempoBaterCarteira = tempoAtual
+            perderEnergia(10)
+        elif chance <= 80 and condicao >= 0:
+            chance2 = random.randint(1, 100)
+            if chance2 < 80:
+                grana = random.randint(10,20)
+            else:
+                grana = random.randint(35,50)
+            player.grana += grana
+            messagebox.showinfo("Ladrãozinho!", "Você conseguiu roubar uma carteira e ganhou "+ str(grana)+ " reais!\n \t-10 de ENERGIA")
+            tempoBaterCarteira = tempoAtual
+            perderEnergia(10)
+    else:
+        messagebox.showinfo("","Aguarde " + str(20-round(resta)) + " segundos")
+    atualizar(energia)
+    
+def pastel():
+    global energia, tempoPastel
+    tempoAtual = time.time()
+    resta = tempoAtual - tempoPastel
+    if  resta >= 20:
+        tempoPastel = 0
+        if player.grana >= 10:
+            if (energia + 50) > player.energiaMax:
+                energia = player.energiaMax
+            else:
+                energia += 50
+            player.grana -= 10
+            tempoPastel = tempoAtual
+        elif player.grana < 10:
+            messagebox.showinfo("Beiçola:","Tá duro? Cai fora!")
+    else:
+        messagebox.showinfo("","Aguarde " + str(20-round(resta)) + " segundos")
+    atualizar(energia)
+
+def inscrever():
+    if player.grana >= 100:
+        player.grana -= 100
+        botaoInscrever.grid_forget()
+        lb_Inscrever.grid_forget()
+        
+        botaoLutar.grid(row=0, column=0, padx=10, pady=10)
+    else:
+        messagebox.showinfo("","Você não tem dinheiro suficiente para inscrever-se no dojo!")
+    
+def lutar():
+    print("lutou")
+        
 
 ## configurando app
 app = tk.Tk()
@@ -109,29 +154,37 @@ nb.place(x=0,y=0,width=400,height=330)
 
 janela=Frame(nb)
 grana=Frame(nb)
+loja=Frame(nb)
+dojo=Frame(nb)
 
 nb.add(janela, text="Inicio")
 nb.add(grana, text="Grana")
+nb.add(loja, text="Loja")
+nb.add(dojo, text="Dojo")
 
 fonte_negrito = font.Font(family="Arial", size=12, weight="bold")
 fonte_label = font.Font(family="Arial", size=4, weight="bold")
-#################################################################
+
+#########################################################################################################################################
 ##configurando stats
+
 stats = Frame()
 stats.pack(side="right")
 stats.pack_propagate(FALSE)
 stats.configure(bg="#39393b", width=100, height=330)
 
+label_nivel = tk.Label(stats, text=player.nivel, font="fonte_negrito", bg=corNivelBg, fg=corNivel)
 label_energia = tk.Label(stats, text="Energia: " + str(energia) + "/" + str(player.energiaMax), font="fonte_negrito", bg="#39393b", fg="yellow")
 label_grana = tk.Label(stats, text="Grana: " + str(player.grana), font="fonte_negrito", bg="#39393b", fg="lime")
 label_poder = tk.Label(stats, text="Poder: " + str(player.poder), font="fonte_negrito", bg="#39393b", fg="red")
 label_forca = tk.Label(stats, text="Força: " + str(player.forca), font="fonte_negrito", bg="#39393b", fg="orange")
 label_agilidade = tk.Label(stats, text="Agilidade: " + str(player.agilidade), font="fonte_negrito", bg="#39393b", fg="cyan")
-label_energia.grid(row=0, padx=15, pady=10)
-label_grana.grid(row=1, padx=15, pady=10)
-label_poder.grid(row=2, padx=15, pady=10)
-label_forca.grid(row=3, padx=15, pady=10)
-label_agilidade.grid(row=4, padx=15, pady=10)
+label_nivel.grid(row=0, padx=15, pady=10)
+label_energia.grid(row=1, padx=15, pady=10)
+label_grana.grid(row=2, padx=15, pady=10)
+label_poder.grid(row=3, padx=15, pady=10)
+label_forca.grid(row=4, padx=15, pady=10)
+label_agilidade.grid(row=5, padx=15, pady=10)
 
 ################################################################
 ##configurando a janela
@@ -147,7 +200,7 @@ botaoAgachamento.grid(row=1, column=0, padx=10, pady=10)
 lb_flexao.grid(row=0, column=1, padx=0, pady=10)
 lb_agachamento.grid(row=1, column=1, padx=0, pady=10)
 
-################################################################
+################################################################################################################################
 ##configurando a grana
 grana.configure(bg="#39393b")
 
@@ -156,6 +209,30 @@ lb_baterCarteira = tk.Label(grana, text="-10 Energia\n 10-50 Grana", bg="#39393b
 
 botaoBaterCarteira.grid(row=0, column=0, padx=10, pady=10)
 lb_baterCarteira.grid(row=0, column=1, padx=0, pady=10)
+
+################################################################################################################################
+##configurando a loja
+
+loja.configure(bg="#39393b", width=300, height=330)
+
+botaoPastel = tk.Button(loja, text="Pastel", command=pastel, width=12, height=2, font="fonte_negrito")
+lb_Pastel = tk.Label(loja, text="-10 Grana\n +50 Energia\n 20s Intervalo", bg="#39393b", fg="lime",font="fonte_label")
+
+botaoPastel.grid(row=0, column=0, padx=10, pady=10)
+lb_Pastel.grid(row=0, column=1, padx=0, pady=10)
+
+################################################################################################################################
+##configurando o dojo
+
+dojo.configure(bg="#39393b", width=300, height=330)
+
+botaoInscrever = tk.Button(dojo, text="Inscrever-se", command=inscrever, width=12, height=2, font="fonte_negrito")
+botaoInscrever.grid(row=0, column=0, padx=10, pady=10)
+
+lb_Inscrever = tk.Label(dojo, text="-100 Grana\n +Acesso ao\nDojo", bg="#39393b", fg="lime",font="fonte_label")
+lb_Inscrever.grid(row=0, column=1, padx=0, pady=10)
+
+botaoLutar = tk.Button(dojo, text="Lutar", command=lutar, width=12, height=2, font="fonte_negrito")
 
 # Iniciar o loop principal da janela
 janela.mainloop()
